520e26c710854502047ba2987c2ad40d56684add
Timbascool <rametukko@gmail.com>
2026-08-02 00:36:15 +0300

    Fix immovable blocks

--- src/main/java/net/glasscrab/iridium/BreakerPushEvent.java
+++ src/main/java/net/glasscrab/iridium/BreakerPushEvent.java
@@ -2,12 +2,10 @@ package net.glasscrab.iridium;
 
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.ItemEnchantments;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Tag;
-import org.bukkit.block.Block;
+import org.bukkit.*;
+import org.bukkit.block.*;
 import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.type.Piston;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -38,19 +36,24 @@ public class BreakerPushEvent implements Listener {
     @EventHandler
     public void onBreakerPush(BlockPistonExtendEvent e){
         var blocks = e.getBlocks();
-        if(blocks.isEmpty()) return;
+        if (blocks.isEmpty()) {
+            return;
+        }
 
         Block piston = e.getBlock();
         Block first = blocks.getFirst();
 
-        if (!Tag.CHAINS.isTagged(first.getType()) && first.getType() != Material.END_ROD) return;
-
+        if (!Tag.CHAINS.isTagged(first.getType()) && first.getType() != Material.END_ROD) {
+            return;
+        }
         Block brokenBlock;
 
         boolean cancelEvent = false;
 
         // Break block right in front of chain
-        if (blocks.size() > 1 && !blocks.get(1).isLiquid()) {
+
+        if (blocks.size() > 1 && !blocks.get(1).isLiquid() &&
+                blocks.get(1).getPistonMoveReaction() != PistonMoveReaction.BREAK) {
             brokenBlock = blocks.get(1);
             cancelEvent = true;
         }
@@ -64,14 +67,14 @@ public class BreakerPushEvent implements Listener {
         if (brokenBlock.getType().getHardness() < 0) return;
         if (brokenBlock.getType().getHardness() >= 50) return;
 
+        if (!isPushable(brokenBlock, e.getDirection(),true, e.getDirection())) return;
+
         // Iron chains transform (when transforming doesn't break), End rods have silk touch and copper chains break normally
         if (first.getType() == Material.IRON_CHAIN &&
-                blockTransformations.containsKey(brokenBlock.getType()))
-        {
+                blockTransformations.containsKey(brokenBlock.getType())) {
             brokenBlock.setType(blockTransformations.get(brokenBlock.getType()));
         }
-        else if (first.getType() == Material.END_ROD)
-        {
+        else if (first.getType() == Material.END_ROD) {
             brokenBlock.breakNaturally(SILK_ITEM,true);
         }
         else {
@@ -80,4 +83,58 @@ public class BreakerPushEvent implements Listener {
 
         if (cancelEvent) e.setCancelled(true);
     }
+
+
+    // Copied from PistonBaseBlock#isPushable() in the Minecraft source code
+    private static boolean isPushable(Block block, BlockFace direction, boolean allowDestroyable, BlockFace connectionDirection)
+    {
+        World world = block.getWorld();
+        Location location = block.getLocation();
+        int y = block.getY();
+
+        if (y < world.getMinHeight() || y > world.getMaxHeight()) {
+            return false;
+        }
+        if (!world.getWorldBorder().isInside(location)) {
+            return false;
+        }
+
+        Material material = block.getType();
+
+        if (material.isAir()) {
+            return true;
+        }
+        if (material == Material.OBSIDIAN || material == Material.CRYING_OBSIDIAN
+            || material == Material.RESPAWN_ANCHOR || material == Material.REINFORCED_DEEPSLATE) {
+            return false;
+        }
+
+        if (direction == BlockFace.DOWN && location.y() == world.getMinHeight()) {
+            return false;
+        }
+        if (direction == BlockFace.UP && location.y() == world.getMaxHeight()) {
+            return false;
+        }
+
+        if (!(material == Material.PISTON || material == Material.STICKY_PISTON)) {
+            if (block.getType().getHardness() == -1) {
+                return false;
+            }
+
+            switch (block.getPistonMoveReaction()) {
+                case BLOCK:
+                    return false;
+                case BREAK:
+                    return allowDestroyable;
+                case PUSH_ONLY:
+                    return direction == connectionDirection;
+            }
+        } else if (((Piston) block.getBlockData()).isExtended()) {
+            return false;
+        }
+
+
+        return !(block.getState() instanceof TileState);
+    }
+
 }
